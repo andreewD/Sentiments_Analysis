19b7f8cfec8af692eb6a16ffd13bbbca5a34cbd0
Andree <hairton.anchi.d@uni.pe>
2020-12-31 10:45:02 -0500

    limpiando archivos

--- prueba.py
+++
@@ -1,8 +0,0 @@
-from textblob import TextBlob
-analysis = TextBlob("This table is black")
-print(analysis.sentiment)
-
-print(analysis.tags)
-print(analysis.translate(to='es'))
-
-print(dir(analysis))
